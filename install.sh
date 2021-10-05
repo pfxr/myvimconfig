35bdcf83aaafc7e2a14cda3ecbc75f9fc1523d73
Pedro Rodrigues <pedro.pfxr@gmail.com>
2021-10-05 21:50:59 +0100

    vim installation and Vundle clone to target dir

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 VIM_CONFIG=`pwd`
 
+apt install vim
+git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+
 cd
 rm -rf .vim
+rm -rf .vimrc
 ln -s $VIM_CONFIG/.vim
+ln -s $VIM_CONFIG/.vimrc
 cd $VIM_CONFIG
